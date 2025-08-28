00bd4d0ab3723ba57a675c064cfd53bbe53fbcc3
HabibieProject <projecthabibie@gmail.com>
2025-08-28 11:16:55 +0700

    Day 25 add token

--- lib/day_25/api/register_user.dart
+++ lib/day_25/api/register_user.dart
@@ -46,10 +46,12 @@ class AuthenticationAPI {
 
   static Future<GetUserModel> updateUser({required String name}) async {
     final url = Uri.parse(Endpoint.profile);
+    final token = await PreferenceHandler.getToken();
+
     final response = await http.post(
       url,
       body: {"name": name},
-      headers: {"Accept": "application/json"},
+      headers: {"Accept": "application/json", "Authorization": token},
     );
     if (response.statusCode == 200) {
       return GetUserModel.fromJson(json.decode(response.body));
