a6fae5c72aa855718ed2f71445ff5070eb413e45
HabibieProject <projecthabibie@gmail.com>
2025-08-28 11:07:43 +0700

    Day 25 API Update

--- lib/day_25/api/register_user.dart
+++ lib/day_25/api/register_user.dart
@@ -26,6 +26,39 @@ class AuthenticationAPI {
     }
   }
 
+  static Future<RegisterUserModel> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    final url = Uri.parse(Endpoint.login);
+    final response = await http.post(
+      url,
+      body: {"email": email, "password": password},
+      headers: {"Accept": "application/json"},
+    );
+    if (response.statusCode == 200) {
+      return RegisterUserModel.fromJson(json.decode(response.body));
+    } else {
+      final error = json.decode(response.body);
+      throw Exception(error["message"] ?? "Register gagal");
+    }
+  }
+
+  static Future<GetUserModel> updateUser({required String name}) async {
+    final url = Uri.parse(Endpoint.profile);
+    final response = await http.post(
+      url,
+      body: {"name": name},
+      headers: {"Accept": "application/json"},
+    );
+    if (response.statusCode == 200) {
+      return GetUserModel.fromJson(json.decode(response.body));
+    } else {
+      final error = json.decode(response.body);
+      throw Exception(error["message"] ?? "Register gagal");
+    }
+  }
+
   static Future<GetUserModel> getProfile() async {
     final url = Uri.parse(Endpoint.profile);
     final token = await PreferenceHandler.getToken();
